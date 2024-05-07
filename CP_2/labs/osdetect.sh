44764936b85a681358d8bcbeb7d9ce4a0026bbab
Ilay Semennikov <semennikovia@gmail.com>
2024-05-07 23:43:29 +0300

    creade winlogs.sh

--- CP_2/labs/osdetect.sh
+++ CP_2/labs/osdetect.sh
@@ -1,24 +1,30 @@
-#! /bin/bash
-#
-#
-# Bash for cybersecurity
-#
-#
-#Description:
-#Detection OS use unamme -s comand
+Linux debian 6.6.15-arm64 #1 SMP Debian 6.6.15-2 (2024-02-04) aarch64
 
-OS_NAME=$(uname -s)
+The programs included with the Debian GNU/Linux system are free software;
+the exact distribution terms for each program are described in the
+individual files in /usr/share/doc/*/copyright.
 
-if [[ "$OS_NAME" = "Linux" ]]
-then
-    echo "You are use Linux OS"
-elif [[ "$OS_NAME" = "Darwin" ]]
-then
-    echo "You are use macOS system"
-elif [[ "$OS_NAME" = "FreeBDS" ]]
-then
-    echo "You are use FreeBDS system"
-else
-    echo "You are use Windows system"
-fi 
-
+Debian GNU/Linux comes with ABSOLUTELY NO WARRANTY, to the extent
+permitted by applicable law.
+Last login: Tue May  7 14:53:09 2024 from 172.16.249.1
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ c^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ jb
+[?2004l-bash: jb: command not found
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ z^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ ^C[?2004l[?2004h[?2004l
+[?2004h]0;sia@debian: ~sia@debian:~$ vy
---
+++ CP_5_Reserch_Information/winlogs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+TGZ=0
+if (( $# >0 ))
+then
+    if [[ ${1:0:2} == '-z' ]]
+    then 
+        TGZ=1 # tgz flags for create archiv logs 
+    shift
+    fi
+fi
+SYSNAM=$(hostname)
+LOGDIR=${1:-/tmp/${SYSNAM}_logs}
+
+
+mkdir -p $LOGDIR
+cd ${LOGDIR} || exit -2
+
+wevtutil el | while read ALOG
+do
+    ALOG="${ALOG%$'\r'}"
+    echo "${ALOG}:"
+    SAFNAM="${ALOG// /_}"
+    SAFNAM="${SAFNAM//\//-}"
+    wevtutil epl "$ALOG" "${SYSNAM}_${SAFNAM}.evtx"
+done
+
+if (( TGZ ==1))
+then
+    tar -czf ${SYSNAM}_logs.tgz *.evtx
+fi
+
+
