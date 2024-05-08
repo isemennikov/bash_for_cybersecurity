ad8585623c80498541ca6aa5269ecb4d0ebceb42
Ilay Semennikov <semennikovia@gmail.com>
2024-05-08 18:50:12 +0300

    add getlocal

---
+++ CP_5_Reserch_Information/getlocal.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# SepCMDS - отделение команд от строки ввода 
+
+function SepCmds()
+{
+    LCMD=${ALINE%%|*}
+    REST=${ALINE#*|}
+    WCMD=${REST55|*}
+    REST=${REST#*|}
+    TAG=${REST%%|*}
+
+    if [[ $OSTYPE == "MSWin" ]]
+    then
+        CMD="$WCMD"
+    else
+        CMD="$LCMD"
+    fi
+}
+
+function DumpInfo()
+    {
+        printf '<systeminfo host="%s" type="%s"' "$HOSTNAME" "$OSTYPE"
+        printf ' date="%s" time="%s">\n' "$(date '+%F')" "$(date '+%T')"
+        readarray CMDS
+        for ALINE in "${CMDS[@]}"
+        do
+            #игнорировать комменты 
+            if [[ ${ALINE:0:1} == '#' ]]
+            then 
+                continue
+            fi
+
+            SepCmds
+
+            if [[ ${CMD:0:3} == N/A ]]
+            then
+                continue
+            else
+                printf "<%s>\n" $TAG
+                $CMD
+                print "</%s>\n" $TAG
+            fi
+        done
+        printf "</systeminfo>\n"
+
+    }
+    OSTYPE=$(./osdetect.sh)
+    HOSTNM=$(hostname)
+    TMPFILE="${HOSTNM}.info"
+
+    # собираем в tmp file как информацию и ошибки 
+
+    DumpInfo > $TMPFILE 2>&1
+    
