c8596bcff02d799050529949571b72418fcaf416
Ilay Semennikov <semennikovia@gmail.com>
2024-05-24 18:37:56 +0300

    labs 5

--- CP_5_Reserch_Information/typesearch.sh
+++ CP_5_Reserch_Information/typesearch.sh
@@ -2,44 +2,47 @@
 #
 #typesearch.sh
 #
+#Description:
+#Searchig any files with scecific type
+#Print path_file if file is found
 #
 #
 #
-#
+#Use:
 #
 DEEPORNOT="-maxdepth 1"  # current directory only; default
 #
 #
-while getopts 'c:irR' opt; do
+while getopts  'c:irR' opt; do                              #1
 
-    case "${opt}" in
+    case "${opt}" in                                        #2
         c) # copy fined files to dir
             COPY=YES
-            DESTDIR="$OPTARG"
+            DESTDIR="$OPTARG"                               #3
             ;;
         i) # ignore case when searching
             CASEMATCH='-i'
             ;;
-        [Rr])
-            unset DEEPORNOT;;
-        *)
+        [Rr])                                               #4
+            unset DEEPORNOT;;                               #5
+        *)                                                  #6
             exist 2;;
     esac
 done
-shift $((OPTIND -1 ))
+shift $((OPTIND -1 ))                                       #7
 
-PATTERN=${1:-PDF document}
+PATTERN=${1:-PDF document}                                  #8
 STARTDIR=${2:-.} #start here by default
 
-find $STARTDIR $DEEPORNOT -type f | while read FN
+find $STARTDIR $DEEPORNOT -type f | while read FN           #9
 do
-    file $FN | egrep -q $CASEMATCH "$PATTERN"
-    if (( $? == 0))
+    file $FN | egrep -q $CASEMATCH "$PATTERN"               #10
+    if (( $? == 0))                                         #11
     then
         echo $FN
-        if [[ $COPY ]]
+        if [[ $COPY ]]                                      #12
         then
-            cp -p $FN $DESTDIR
+            cp -p $FN $DESTDIR                              #13
         fi
     fi
 done
